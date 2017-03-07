e41c6d8023bb997171b5f42cf7a95007a16210d2
mcataford <c.marcandre@gmail.com>
2017-03-07 17:43:12 -0500

    Update.

---
+++ Winter 2017/Java/In-class exercises/PalindromeCheck.java
@@ -0,0 +1,11 @@
+public class PalindromeCheck {
+	public static void main(String[] args) {
+		String[] words = {"laval", "hello", "banana"};
+		
+		//Your code here.
+	}
+	
+	public static boolean isPalindrome(String theWord) {
+		//Insert your logic here.
+	}
+}
